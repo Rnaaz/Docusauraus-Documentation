e50a3ec8171e3318c9e1aa33705887034c909b30
rnaaz <rohinanaaz@gmail.com>
2021-11-16 12:17:10 +0530

    Updated for IUDX Documentation

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'IUDX Documentation',
+  tagline: 'Learn about exploring, building , deploying your apps with IUDX',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -43,10 +43,10 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: '',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/iudx_logo.png',
         },
         items: [
           {

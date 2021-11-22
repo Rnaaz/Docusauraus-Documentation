fd9ebcc2847d6212b54a8432627e9fbb52fb4a1d
rnaaz <rohinanaaz@gmail.com>
2021-11-22 14:17:04 +0530

    Changed the config file

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -25,13 +25,13 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+          editUrl: 'https://github.com/Rnaaz/Docusauraus-Documentation/tree/main/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+            'https://github.com/Rnaaz/Docusauraus-Documentation/tree/main/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -58,7 +58,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/Rnaaz/Docusauraus-Documentation',
             label: 'GitHub',
             position: 'right',
           },
@@ -102,7 +102,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/Rnaaz/Docusauraus-Documentation',
               },
             ],
           },
